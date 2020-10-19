da658a3b14e84a11248369879788b21827bac8ad
matheusssb <48969059+matheusssb@users.noreply.github.com>
2020-10-19 08:58:44 -0300

    Update Base.js

--- Base.js
+++ Base.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Form, Button,Nav,Navbar} from "react-bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FooterApp from "./Footer/Footer"
 import NavbarApp from "./Navbar/Navbar"
