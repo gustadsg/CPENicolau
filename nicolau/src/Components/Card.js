76efb86dbb51de66613c8a4b88cad66620c3206f
gustadsg <gustadsg@outlook.com.br>
2020-10-21 23:07:42 -0300

    modal criado

--- nicolau/src/Components/Card.js
+++ nicolau/src/Components/Card.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Modal from 'react-modal'
 import '../css/bootstrap.min.css'
 import '../css/card.css'
 import {Link} from "react-router-dom"
 
 export default function Card (props){
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+    Modal.setAppElement('#root');
+
     return(
         <>
         <div className="card">
@@ -11,7 +15,7 @@ export default function Card (props){
             <div className="card-body">
                 <h5 className="card-title">{props.name}</h5>
                 {/* <!-- description --> */}
-                <p className="card-text">{props.children}</p>
+                
                 {/* <!-- info --> */}
                 <ul className="list-group list-group-flush">
                     {/* <!-- this is just for the border --> */}
@@ -23,12 +27,37 @@ export default function Card (props){
                     <li className="list-group-item"></li>
                 </ul>
                 {/* <!-- adopt button --> */}
-                <Link className="btn btn-primary" to="/Formulario">Adote-me</Link>
+                <div className="btn-container">
+                    <button className="btn btn-warning " onClick={()=>setModalIsOpen(true)}>Ver mais</button>
+                    <Link className="btn btn-primary  adoption-btn" to="/Formulario" >Adote-me</Link>
+                </div>
                 
             </div>
         </div>
 
 
+        {/* modal goes here */}
+
+            <Modal isOpen={modalIsOpen}>
+                <div className="img-container">
+                    <img src={require('../assets/' + props.name + '.png')} className="modal-img" alt={"foto de " + props.name}/>
+                </div>
+                <div className="modal-body">
+                    <h2 className="modal-title"> {props.name} </h2>
+                        <p>{props.children}</p>
+
+                        <ul className="modal-ul">
+                            <li>{props.race}</li>
+                            <li>{props.gender}</li>
+                            <li>{props.age} meses</li>
+                        </ul>
+                </div>
+                <div className="btn-container">
+                    <Link className="btn btn-primary btn-lg adoption-btn" to="/Formulario" >Adote-me</Link>
+                    <button className="btn btn-danger btn-lg close-btn" onClick={()=> setModalIsOpen(false)}>Fechar</button>
+                </div>
+                
+            </Modal>
         </>
     )
 }
