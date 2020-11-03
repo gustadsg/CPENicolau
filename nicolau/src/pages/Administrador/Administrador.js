daa774dc9344feb63e057109f7850a7049db3a89
gustadsg <gustadsg@outlook.com.br>
2020-11-03 10:41:44 -0300

    criaçao da pagina de admin

---
+++ nicolau/src/pages/Administrador/Administrador.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Base from '../../Components/Base'
+import Candidat from '../../Components/Candidat'
+import '../../css/administrador.css'
+
+export default function Admnistrador(){
+
+
+    return (
+        <Base>
+            <div className='container-flex'>
+                <Candidat/>
+                <Candidat/>
+                <Candidat/>
+                <Candidat/>
+                <Candidat/>
+            </div>
+        </Base>
+    )
+}
